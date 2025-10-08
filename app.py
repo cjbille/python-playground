a1e920b74e004659ba1e8fce5453da3dbade6612
Chris Bille <cbille1@icloud.com>
2025-10-08 19:23:30 -0400

    Updates

---
+++ app.py
@@ -0,0 +1,18 @@
+# create python virtual environment
+# python3 -m venv .venv
+#
+# activate python virtual environment
+# .venv/bin/activate
+#
+# leave python virtual environment
+# deactivate
+#
+# freeze pip requirements
+# pip freeze > requirements.txt
+#
+# install pip requirements
+# pip install -r requirements.txt
+#
+# code begins here #
+#
+
