d6f874b8a54774342f603f646e9b2467393b553b
Chris Bille <cbille1@icloud.com>
2025-10-09 19:27:51 -0400

    Updates

--- app.py
+++ app.py
@@ -15,6 +15,9 @@
 #
 # code begins here #
 #
+import math
+from decimal import Decimal, getcontext
+
 print("hello world")
 name = "chris"
 one = 1
@@ -56,3 +59,21 @@ class Cat:
 
 my_cat = Cat("Fuzzlekins")
 print(my_cat.speak())
+
+print(int(8.999)) # python will round down to 8, use round function
+print(round(14/3, 2))
+
+getcontext().prec = 3
+print(Decimal(1) / Decimal(3))
+
+name = "my name is fuzzlekins"
+print(name[0])
+print(name[:7])
+print(name[5:])
+print(len(name))
+print(f'number is {4}') # prior to python 3.6, you had use string.format()
+print('Pi is {}'.format(math.pi))
+
+print(bytes(4))
+print(bytes('😃', 'utf-8'))
+print(bytes('😃', 'utf-8').decode('utf-8'))
