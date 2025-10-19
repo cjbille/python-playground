a03b9306f106d81f87a261a15a47b35be6012d59
Chris Bille <cbille1@icloud.com>
2025-10-19 09:40:26 -0400

    Updates

--- numbers/__init__.py
+++ numbers/__init__.py
@@ -1,1 +1,3 @@
 # this is important, it tells python this folder is a package
+
+print(f'__init__.py module name is {__name__}')
--- primes.py
+++ primes.py
@@ -12,4 +12,11 @@ def listPrimes(max):
         if isPrime(n, foundPrimes):
             foundPrimes.append(n)
     return foundPrimes
-    
+
+print(f'primes.py module name is {__name__}')
+
+# __name__ is a builtin python variable
+# the line below says if __name__ is equal to __main__, that means this module is being
+# called directly (ie: python3 primes.py), and the code in the if statement will execute
+if __name__ == '__main__':
+    print("this is a module, please import using primes")
